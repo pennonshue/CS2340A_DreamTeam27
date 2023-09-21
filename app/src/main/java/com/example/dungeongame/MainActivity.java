6e4e589bf05c49dd73313ba0519d2388751d021d
aidanwu04 <aidanwu04@gmail.com>
2023-09-21 01:01:27 -0400

    Fixed connectivity between screens and buttons

--- app/src/main/java/com/example/dungeongame/MainActivity.java
+++ app/src/main/java/com/example/dungeongame/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-        start = (Button) findViewById(R.id.startBtn);
+        Button start = findViewById(R.id.startBtn);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
