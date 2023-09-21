bd66f28395aa390f5e2f855d2f10bc42ed423049
robster165 <139757760+robster165@users.noreply.github.com>
2023-09-21 14:45:06 -0400

    fixed the name

--- app/src/main/java/com/example/dungeongame/SetupActivity.java
+++ app/src/main/java/com/example/dungeongame/SetupActivity.java
@@ -6,22 +6,41 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SetupActivity extends AppCompatActivity {
+
+    private TextInputEditText nameEditText;
+    private RadioGroup playerRadioGroup;
+    private RadioButton selectedPlayerRadioButton;
+    private Button continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
 
-        Button cont = findViewById(R.id.contBtn);
-        cont.setOnClickListener(new View.OnClickListener() {
+        // Initialize views
+        nameEditText = findViewById(R.id.nameBox);
+        continueButton = findViewById(R.id.contBtn);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SetupActivity.this, GameScreen.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                // Get the user input from the EditText
+                String playerName = nameEditText.getText().toString().trim();
+
+                // Check if the input is null, empty, or whitespace-only
+                if (playerName.isEmpty() || playerName.matches("^\\s*$")) {
+                    Toast.makeText(SetupActivity.this, "Please enter a valid name", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Name and player selection are valid, proceed to the next activity
+                    Intent intent = new Intent(SetupActivity.this, GameScreen.class); // Replace NextActivity with your desired destination
+                    intent.putExtra("playerName", playerName);
+                    startActivity(intent);
+                }
             }
         });
     }
