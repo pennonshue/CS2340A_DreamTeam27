6773adc50fad2c7fb5388ef33b43ee1ca62ee64e
Pennon <pennonshue@gmail.com>
2023-09-20 22:39:55 -0400

    Created setup screen and new setup activity file

--- app/src/main/java/com/example/dungeongame/MainActivity.java
+++ app/src/main/java/com/example/dungeongame/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.dungeongame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.startBtn);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SetupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/dungeongame/SetupActivity.java
+++ app/src/main/java/com/example/dungeongame/SetupActivity.java
@@ -1,2 +1,19 @@
-package com.example.dungeongame;public class SetupActivity {
-}
+package com.example.dungeongame;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SetupActivity extends AppCompatActivity{
+        Button start;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_setup);
+
+
+        }
+
+    }
