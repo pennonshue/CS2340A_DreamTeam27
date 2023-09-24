d1f2b5ab9fb902d4fd3bb3fdd5c6a74b2fa2307a
nikvijay07 <nikvijay07@gmail.com>
2023-09-24 13:30:36 -0400

    Adding username to game screen relative to player

--- app/src/main/java/com/example/dungeongame/GameScreen.java
+++ app/src/main/java/com/example/dungeongame/GameScreen.java
@@ -1,10 +1,13 @@
 package com.example.dungeongame;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -33,6 +36,39 @@ public class GameScreen extends AppCompatActivity {
         layoutParams.topMargin = 380;
 
         gameViewSprite.setLayoutParams(layoutParams);
+
+
+        TextView usernameTextView = new TextView(this);
+        System.out.println(User.getUsername());
+        usernameTextView.setText(User.getUsername());
+        usernameTextView.setTextSize(20); // Adjust text size as needed
+        usernameTextView.setTextColor(Color.BLACK);
+
+        FrameLayout.LayoutParams usernameParams = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT
+        );
+
+// Calculate the position for the username TextView based on the sprite's position
+        int spriteX = (int) gameViewSprite.getX();
+
+        int spriteY = (int) gameViewSprite.getY();
+        System.out.println(spriteX + "," + spriteY);
+
+        int usernameWidth = usernameTextView.getMeasuredWidth();
+
+
+        int usernameX = (int) spriteX + 350 - usernameWidth/2; // Adjust this as needed
+        int usernameY = (int) (spriteY + 440); // Adjust this as needed to position it above the sprite
+        System.out.println(usernameX +"," + usernameY);
+
+// Set the position of the username TextView
+        usernameParams.leftMargin = usernameX;
+        usernameParams.topMargin = usernameY;
+
+// Add the username TextView to the FrameLayout
+        characterSprite.addView(usernameTextView, usernameParams);
+
 
 
         Button end = findViewById(R.id.EndBut);
