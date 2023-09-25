e12c496d176710178003625002080156be22e361
robster165 <139757760+robster165@users.noreply.github.com>
2023-09-24 21:11:40 -0400

    made gamescreen better?

--- app/src/main/java/com/example/dungeongame/GameScreen.java
+++ app/src/main/java/com/example/dungeongame/GameScreen.java
@@ -3,6 +3,7 @@ package com.example.dungeongame;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -27,120 +28,52 @@ public class GameScreen extends AppCompatActivity {
 
         characterSprite.addView(gameViewSprite);
 
-        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.WRAP_CONTENT, // Width (or specify a fixed width)
-                FrameLayout.LayoutParams.WRAP_CONTENT  // Height (or specify a fixed height)
-        );
-
-        layoutParams.leftMargin = 170;
-        layoutParams.topMargin = 380;
-
-        gameViewSprite.setLayoutParams(layoutParams);
-
-
-        TextView usernameTextView = new TextView(this);
-        System.out.println(User.getUsername());
-        usernameTextView.setText(User.getUsername());
-        usernameTextView.setTextSize(20); // Adjust text size as needed
-        usernameTextView.setTextColor(Color.BLACK);
-
-        FrameLayout.LayoutParams usernameParams = new FrameLayout.LayoutParams(
+        FrameLayout.LayoutParams spriteLayoutParams = new FrameLayout.LayoutParams(
                 FrameLayout.LayoutParams.WRAP_CONTENT,
                 FrameLayout.LayoutParams.WRAP_CONTENT
         );
 
-        // Calculate the position for the username TextView based on the sprite's position
+        // Center the sprite in the middle of the screen
+        spriteLayoutParams.gravity = Gravity.CENTER;
+        gameViewSprite.setLayoutParams(spriteLayoutParams);
 
-        int spriteX = (int) gameViewSprite.getX();
+        // Create a LinearLayout to hold the player information
+        LinearLayout infoLayout = new LinearLayout(this);
+        infoLayout.setOrientation(LinearLayout.HORIZONTAL);
+        infoLayout.setGravity(Gravity.CENTER);
 
-        int spriteY = (int) gameViewSprite.getY();
-        System.out.println(spriteX + "," + spriteY);
+        // Create TextViews for player name, difficulty, and health
+        TextView playerNameTextView = new TextView(this);
+        playerNameTextView.setText("Name: " + User.getUsername());
+        playerNameTextView.setTextSize(20);
+        playerNameTextView.setTextColor(Color.BLACK);
 
-        int usernameWidth = usernameTextView.getMeasuredWidth();
+        TextView difficultyTextView = new TextView(this);
+        difficultyTextView.setText("Difficulty: " + User.getDifficulty());
+        difficultyTextView.setTextSize(20);
+        difficultyTextView.setTextColor(Color.BLACK);
 
-
-        int usernameX = (int) spriteX + 350 - usernameWidth / 2; // Adjust this as needed
-        int usernameY = (int) (spriteY + 440); // Adjust this to position it above the sprite
-        System.out.println(usernameX + "," + usernameY);
-
-        // Set the position of the username TextView
-        usernameParams.leftMargin = usernameX;
-        usernameParams.topMargin = usernameY;
-
-        // Add the username TextView to the FrameLayout
-        characterSprite.addView(usernameTextView, usernameParams);
-
-        // Get the user's health as an integer
-        int userHealth = User.getHealth();
-
-        // Convert the integer to a String
-        String healthText = String.valueOf(userHealth);
-
-        // Create a TextView for health
         TextView healthTextView = new TextView(this);
-        healthTextView.setText(healthText);
-        healthTextView.setTextSize(20); // Adjust text size as needed
+        healthTextView.setText("HP: " + User.getHealth());
+        healthTextView.setTextSize(20);
         healthTextView.setTextColor(Color.BLACK);
 
-        String difficulty = User.getDifficulty();
+        // Add TextViews to the LinearLayout
+        infoLayout.addView(playerNameTextView);
+        infoLayout.addView(difficultyTextView);
+        infoLayout.addView(healthTextView);
 
-        // Convert the integer to a String
-
-        // Create a TextView for health
-        TextView difficultyTextView = new TextView(this);
-        difficultyTextView.setText(difficulty);
-        difficultyTextView.setTextSize(20); // Adjust text size as needed
-        difficultyTextView.setTextColor(Color.BLACK);
-        // Create a parent LinearLayout to hold both health and difficulty TextViews
-        LinearLayout parentLayout = new LinearLayout(this);
-        parentLayout.setOrientation(LinearLayout.HORIZONTAL);
-
-
-// Add health and difficulty TextViews to the parent LinearLayout
-        parentLayout.addView(healthTextView);
-        parentLayout.addView(difficultyTextView);
-
-// Add the parent LinearLayout to the FrameLayout
-        FrameLayout.LayoutParams myParam = new FrameLayout.LayoutParams(
+        // Add the LinearLayout to the top of the screen
+        FrameLayout.LayoutParams infoLayoutParams = new FrameLayout.LayoutParams(
                 FrameLayout.LayoutParams.WRAP_CONTENT,
                 FrameLayout.LayoutParams.WRAP_CONTENT
         );
-        characterSprite.addView(parentLayout, myParam);
+        infoLayoutParams.gravity = Gravity.TOP | Gravity.CENTER_HORIZONTAL;
+        characterSprite.addView(infoLayout, infoLayoutParams);
 
-// Calculate the position for the parent LinearLayout based on the sprite's position
-        int parentWidth = parentLayout.getMeasuredWidth();
-
-        int parentX = (int) spriteX + 600 - parentWidth / 2; // Adjust this as needed
-        int parentY = (int) (spriteY + 440); // Adjust this to position it above the sprite
-        System.out.println(parentX + " , " + parentY);
-
-// Set the position of the parent LinearLayout
-        myParam.leftMargin = parentX;
-        myParam.topMargin = parentY;
-
-
-//        // Add the health TextView to the FrameLayout
-//        FrameLayout.LayoutParams healthParams = new FrameLayout.LayoutParams(
-//                FrameLayout.LayoutParams.WRAP_CONTENT,
-//                FrameLayout.LayoutParams.WRAP_CONTENT
-//        );
-//
-//        characterSprite.addView(healthTextView, healthParams);
-//
-//        // Calculate the position for the health TextView based on the sprite's position
-//        int healthWidth = healthTextView.getMeasuredWidth();
-//
-//        int healthX = (int) spriteX + 600 - healthWidth / 2; // Adjust this as needed
-//        int healthY = (int) (spriteY + 440); // Adjust this to position it above the sprite
-//        System.out.println(healthX + " , " + healthY);
-//
-//        // Set the position of the health TextView
-//        healthParams.leftMargin = healthX;
-//        healthParams.topMargin = healthY;
-
-
-        Button end = findViewById(R.id.EndBut);
-        end.setOnClickListener(new View.OnClickListener() {
+        // Set an OnClickListener for the End Button
+        Button endButton = findViewById(R.id.EndBut);
+        endButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(GameScreen.this, EndScreen.class);
