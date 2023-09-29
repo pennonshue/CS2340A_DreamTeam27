2efa4ae945cc55c6d66d9f439922fd388479f4c3
Pennon <pennonshue@gmail.com>
2023-09-29 16:42:39 -0400

    created new game screens and buttons

---
+++ app/src/main/java/com/example/dungeongame/GameScreen2.java
@@ -0,0 +1,24 @@
+package com.example.dungeongame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameScreen2 extends AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_2);
+
+        Button toScreen3Btn = findViewById(R.id.toScreen3Btn);
+        toScreen3Btn.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(GameScreen2.this, GameScreen3.class);
+            startActivity(intent);
+        }
+    });
+}
+}
--- app/src/main/java/com/example/dungeongame/GameScreen3.java
+++ app/src/main/java/com/example/dungeongame/GameScreen3.java
@@ -4,25 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-public class EndScreen extends AppCompatActivity {
-    @Override
+public class GameScreen3 extends AppCompatActivity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_end);
+        setContentView(R.layout.activity_game_3);
 
-        Button playAgainButton = findViewById(R.id.playAgainButton);
-        playAgainButton.setOnClickListener(new View.OnClickListener() {
+        Button endBtn = findViewById(R.id.EndBut);
+        endBtn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(EndScreen.this, MainActivity.class);
+                Intent intent = new Intent(GameScreen3.this, EndScreen.class);
                 startActivity(intent);
             }
         });
     }
-
-
 }
