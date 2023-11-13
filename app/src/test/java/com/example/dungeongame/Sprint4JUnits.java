3c85d16a90bc026dd8c9c1db48ce94d25bf9f972
aidanwu04 <aidanwu04@gmail.com>
2023-11-13 13:56:00 -0500

    Updates

--- app/src/test/java/com/example/dungeongame/Sprint4JUnits.java
+++ app/src/test/java/com/example/dungeongame/Sprint4JUnits.java
@@ -1,64 +1,93 @@
 package com.example.dungeongame;
-
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import android.content.Context;
-
-import com.example.dungeongame.model.BossFactory;
 import com.example.dungeongame.model.Player;
-import com.example.dungeongame.model.User;
-import com.example.dungeongame.views.GameScreen;
+
 
 import org.junit.Test;
 
 public class Sprint4JUnits {
     @Test
-    public void collisionTest() {
-
+    public void changeHealth() {
+        Player.resetPlayer();
+        Player player1 = Player.getInstance("Steve", 1, "Easy");
+        Player.setHealth(Player.getHealth() - 1);
+        assertEquals(Player.getHealth(), 99);
     }
     @Test
-    public void BossCreator() {
-
+    public void resetPlayer() {
+        Player player1 = Player.getInstance("Steve", 1, "Easy");
+        Player.resetPlayer();
+        assertEquals(Player.getInstance(), null);
     }
     @Test
-    public void PurpleManCreator() {
-
+    public void PlayerHealthNoNegative() {
+        Player.resetPlayer();
+        Player player1 = Player.getInstance("Steve", 1, "Easy");
+        Player.setHealth(Player.getHealth() - 101);
+        assertEquals(Player.getHealth(), 0);
     }
     @Test
-    public void NecromancerCreator() {
-
+    public void checkScoreWhenDie() {
+        Player.resetPlayer();
+        Player player1 = Player.getInstance("Steve", 1, "Easy");
+        Player.setHealth(Player.getHealth() - 101);
+        assertEquals(Player.getScore(), 0);
     }
     @Test
-    public void CreatureCreator() {
-
+    public void DifficultyEasyDamage() {
+        Player.resetPlayer();
+        Player player1 = Player.getInstance("Steve", 1, "Easy");
+        Player.collisionDamage();
+        assertEquals(Player.getHealth(), 90);
     }
     @Test
-    public void GooberCreator() {
-
+    public void DifficultyMedDamage() {
+        Player.resetPlayer();
+        Player player1 = Player.getInstance("Steve", 1, "Medium");
+        Player.collisionDamage();
+        assertEquals(Player.getHealth(), 70);
     }
     @Test
-    public void KnightCreator() {
-
+    public void DifficultyHardDamage() {
+        Player.resetPlayer();
+        Player player1 = Player.getInstance("Steve", 1, "Hard");
+        Player.collisionDamage();
+        assertEquals(Player.getHealth(), 40);
     }
     @Test
-    public void KnightHealth() {
-
+    public void CheckScoreNoDie() {
+        Player.resetPlayer();
+        Player player1 = Player.getInstance("Steve", 1, "Easy");
+        Player.setHealth(Player.getHealth() - 50);
+        assertEquals(Player.getScore(), 20);
     }
     @Test
-    public void PurpleManHealth() {
-
+    public void checkCollisionTrue() {
+        Player.resetPlayer();
+        Player player1 = Player.getInstance("Steve", 1, "Easy");
+        assertTrue(Player.ifCollide(100,100));
     }
     @Test
-    public void NecromancerHealth() {
-
+    public void checkCollisionFalse() {
+        Player.resetPlayer();
+        Player player1 = Player.getInstance("Steve", 1, "Easy");
+        assertEquals(Player.ifCollide(50,50),false);
     }
     @Test
-    public void GooberHealth() {
-
+    public void checkCollisionCauseDamageTrue() {
+        Player.resetPlayer();
+        Player player1 = Player.getInstance("Steve", 1, "Easy");
+        Player.ifCollide(100, 100);
+        assertEquals(Player.getHealth(), 90);
     }
     @Test
-    public void CreatureHealth() {
-
+    public void checkCollisionCauseDamageFalse() {
+        Player.resetPlayer();
+        Player player1 = Player.getInstance("Steve", 1, "Easy");
+        Player.ifCollide(99, 100);
+        assertEquals(Player.getHealth(), 100);
     }
 
 }
