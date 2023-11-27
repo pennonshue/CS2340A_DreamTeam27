71c5d317e2858720744524e9fa7b5210a7c450bb
jasonli04 <lijason1204@gmail.com>
2023-11-26 22:34:38 -0500

    speed updates

--- app/src/main/java/com/example/dungeongame/model/RunStrategy.java
+++ app/src/main/java/com/example/dungeongame/model/RunStrategy.java
@@ -2,6 +2,7 @@ package com.example.dungeongame.model;
 
 
 public class RunStrategy implements MovementStrategy {
+    //sets run speed
     public int movementSpeed() {
         return 30;
     }
--- app/src/main/java/com/example/dungeongame/model/SprintStrategy.java
+++ app/src/main/java/com/example/dungeongame/model/SprintStrategy.java
@@ -1,6 +1,7 @@
 package com.example.dungeongame.model;
 
 public class SprintStrategy implements MovementStrategy {
+    //sets sprint speed
     public int movementSpeed() {
         return 60;
     }
